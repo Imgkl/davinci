486396ad75de4d0a1db5b09440b2059b291f1b19
saigkl <gokul@1stmain.co>
2021-05-03 01:06:54 +0530

    dart formatted

--- lib/core/davinci_capture.dart
+++ lib/core/davinci_capture.dart
@@ -6,21 +6,42 @@ import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DavinciCapture {
+  /// click method is defined.
+  /// if the fileName is not set, it sets the file name as "davinci".
   static Future click(GlobalKey key, {String fileName = "davinci"}) async {
+    /// if the key is null, it returns null
     if (key == null) return;
     try {
+      /// finding the widget in the current context by the key.
       RenderRepaintBoundary boundary = key.currentContext.findRenderObject();
+
+      /// the boundary is converted to Image.
       final image = await boundary.toImage(pixelRatio: 3);
+
+      /// The raw image is converted to byte data.
       final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+
+      /// The byteData is converted to uInt8List image aka memory Image.
       final u8Image = byteData.buffer.asUint8List();
+
+      /// getting the temp directory of the app.
       String dir = (await getApplicationDocumentsDirectory()).path;
+
+      /// Saving the file with the file name in temp directory.
       File file = new File('$dir/$fileName.png');
+
+      /// the image file is created
       await file.writeAsBytes(u8Image);
+
+      /// The image file is opened.
       OpenFile.open(
         '$dir/$fileName.png',
       );
+
+      /// The file is returned.
       return file;
     } catch (e) {
+      /// if the above process is failed, the error is printed.
       print(e);
     }
   }
--- lib/core/davinci_core.dart
+++ lib/core/davinci_core.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Davinci extends StatefulWidget {
+  /// builder is passed along with the key.
   final Function(GlobalKey key) builder;
 
-  const Davinci({Key key, this.builder}) : super(key: key);
+  const Davinci({this.builder});
   @override
   _DavinciState createState() => _DavinciState();
 }
 
 class _DavinciState extends State<Davinci> {
-  final globalkey = GlobalKey();
+  /// global key is defined which is set the the RepaintBoundary.
+  final GlobalKey globalKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
+    /// this is the widget which will help the plugin to understand and
+    /// identify which widget has to be selected and turned into image.
     return RepaintBoundary(
-      key: globalkey,
-      child: widget.builder(globalkey),
+      /// the global key is key which is set in State variable
+      key: globalKey,
+
+      /// the children is what passed in, along with the key.
+      child: widget.builder(globalKey),
     );
   }
 }
