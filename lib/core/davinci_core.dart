e54923399e27b7e2d8d918cd031b8d8f12a5c3cd
saigkl <gokul@1stmain.co>
2021-05-12 11:45:57 +0530

    added proper null safety and added capture parameters

--- lib/core/davinci_core.dart
+++ lib/core/davinci_core.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Davinci extends StatefulWidget {
+class Davinci extends StatelessWidget {
   /// builder is passed along with the key.
   final Function(GlobalKey key)? builder;
 
   const Davinci({this.builder});
-  @override
-  _DavinciState createState() => _DavinciState();
-}
-
-class _DavinciState extends State<Davinci> {
-  /// global key is defined which is set the the RepaintBoundary.
-  final GlobalKey globalKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
+    /// Getting the global key which will be used to passed to RepaintBoundary
+    final GlobalKey globalKey = GlobalKey();
+
+    /// global key is defined which is set the the RepaintBoundary.
     /// this is the widget which will help the plugin to understand and
     /// identify which widget has to be selected and turned into image.
+
     return RepaintBoundary(
       /// the global key is key which is set in State variable
       key: globalKey,
 
       /// the children is what passed in, along with the key.
-      child: widget.builder!(globalKey),
+      child: builder!(globalKey),
     );
   }
 }
