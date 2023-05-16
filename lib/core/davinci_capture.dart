cd5f6d12783be0ae49bd7006e0df95835881ab87
0x0-14 <saigklkrishnan@gmail.com>
2023-05-16 21:19:30 +0530

    wip

--- lib/core/davinci_capture.dart
+++ lib/core/davinci_capture.dart
@@ -81,7 +81,7 @@ class DavinciCapture {
     pixelRatio ??= View.of(context).devicePixelRatio;
     try {
       final RenderView renderView = RenderView(
-        view: View.of(context),
+        window: View.of(context),
         child: RenderPositionedBox(
             alignment: Alignment.center, child: repaintBoundary),
         configuration: ViewConfiguration(
